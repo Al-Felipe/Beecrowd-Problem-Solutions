fc5f92200ebe3f149653867286384ff0ff9df2f5
Al-Felipe <105646899+Al-Felipe@users.noreply.github.com>
2024-09-11 08:14:45 -0300

    Update 1072 - Intervalo 2.java

--- 1072 - Intervalo 2.java
+++ 1072 - Intervalo 2.java
@@ -1,12 +1,8 @@
-import java.util.Scanner;
-
-public class Intervalo2 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
 //        Leia um valor inteiro N. Este valor será a quantidade de valores inteiros X que serão lidos em seguida.
 //        Mostre quantos destes valores X estão dentro do intervalo [10,20] e quantos estão fora do intervalo,
 //                mostrando essas informações.
+        
+    Scanner sc = new Scanner(System.in);
     int n = sc.nextInt();
     int in = 0;
     int out = 0;
