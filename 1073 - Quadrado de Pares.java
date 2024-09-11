a08709a29eea144b0eea1b6e46eec12f6d1860fd
Al-Felipe <105646899+Al-Felipe@users.noreply.github.com>
2024-09-11 08:15:59 -0300

    Update 1073 - Quadrado de Pares.java

--- 1073 - Quadrado de Pares.java
+++ 1073 - Quadrado de Pares.java
@@ -1,4 +1,6 @@
-  Scanner sc = new Scanner(System.in);
+// Leia um valor inteiro N. Apresente o quadrado de cada um dos valores pares, de 1 até N, inclusive N, se for o caso.
+
+Scanner sc = new Scanner(System.in);
         int num = sc.nextInt();
         for (int i = 1; i <= num; i++) {
             if (i % 2 == 0){
