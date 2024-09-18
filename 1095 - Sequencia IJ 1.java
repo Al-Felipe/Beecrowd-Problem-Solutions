33c355b0f902ed7fca7491968a1ff68d45dfb89b
Al-Felipe <105646899+Al-Felipe@users.noreply.github.com>
2024-09-18 08:40:32 -0300

    Create 1095 - Sequencia IJ 1.java

---
+++ 1095 - Sequencia IJ 1.java
@@ -0,0 +1,10 @@
+// Você deve fazer um programa que apresente a sequencia conforme o exemplo abaixo.
+
+        int j = 60;
+        int i = 1;
+
+        do {
+            System.out.println("I=" + i + " J=" + j);
+            i += 3;
+            j -= 5;
+        } while (j >= 0);
