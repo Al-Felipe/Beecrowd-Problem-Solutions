529e9a17a8bd3c1cfe1e19416fdc0eeeb11b17eb
Al-Felipe <105646899+Al-Felipe@users.noreply.github.com>
2024-09-18 10:00:01 -0300

    Create 1096 - Sequencia IJ 2.java

---
+++ 1096 - Sequencia IJ 2.java
@@ -0,0 +1,9 @@
+// Você deve fazer um programa que apresente a sequencia conforme o exemplo abaixo.
+
+        int i = 1;
+        while (i <= 9) {
+          for (int j = 7; j >= 5; j--) {
+                System.out.println("I=" + i + " J=" + j);
+            }
+            i += 2;
+        }
