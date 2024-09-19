a9cf99f7b112427c5937380a6a42c18aaacd1801
Al-Felipe <105646899+Al-Felipe@users.noreply.github.com>
2024-09-19 08:42:30 -0300

    Update 1096 - Sequencia IJ 2.java

--- 1096 - Sequencia IJ 2.java
+++ 1096 - Sequencia IJ 2.java
@@ -1,4 +1,15 @@
-// Você deve fazer um programa que apresente a sequencia conforme o exemplo abaixo.
+/* Você deve fazer um programa que apresente a sequencia conforme o exemplo abaixo.
+I=1 J=7
+I=1 J=6
+I=1 J=5
+I=3 J=7
+I=3 J=6
+I=3 J=5
+...
+I=9 J=7
+I=9 J=6
+I=9 J=5
+*/
 
         int i = 1;
         while (i <= 9) {
