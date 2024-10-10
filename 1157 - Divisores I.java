a6b78c6b6a1e7509ae6197e145321d212f2fd3c2
Al-Felipe <105646899+Al-Felipe@users.noreply.github.com>
2024-10-10 20:09:14 -0300

    Create 1157 - Divisores I.java

---
+++ 1157 - Divisores I.java
@@ -0,0 +1,7 @@
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        for (int i = 1; i <= x ; i++) {
+            if (x % i == 0){
+                System.out.println(i);
+            }
+        }
